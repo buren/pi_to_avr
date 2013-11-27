1e4b21870a42c822afb2b2ae7346b3f9e06d752b
Jacob Burenstam <burenstam@gmail.com>
2013-11-27 17:12:13 +0100

    update

--- TwoWaySerialComm.java
+++ TwoWaySerialComm.java
@@ -54,7 +54,7 @@ public class TwoWaySerialComm {
       byte[] buffer = new byte[ 1024 ];
       int len = -1;
       try {
-        while( ( len = this.in.read( buffer ) ) &gt; -1 ) {
+        while( ( len = this.in.read( buffer ) ) > -1 ) {
           System.out.print( new String( buffer, 0, len ) );
         }
       } catch( IOException e ) {
@@ -74,7 +74,7 @@ public class TwoWaySerialComm {
     public void run() {
       try {
         int c = 0;
-        while( ( c = System.in.read() ) &gt; -1 ) {
+        while( ( c = System.in.read() ) > -1 ) {
           this.out.write( c );
         }
       } catch( IOException e ) {
